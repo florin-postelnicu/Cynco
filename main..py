c609a05a5d0c45687b9acff9f23d2d0a5e1aec47
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2018-12-21 15:46:27 -0700

    Add files via upload

---
+++ main..py
@@ -0,0 +1,13 @@
+
+from aiohttp import web
+
+async def index(request):
+    return web.Response(text = 'Hello Aiohttp')
+
+def setup_routes(app):
+    app.router.add_get('/', index)
+
+app = web.Application()
+setup_routes(app)
+web.run_app(app, host='127.0.0.1', port=8880)
+
